5345c6ddcd34df73ee5b0057cb2c324cdcda9371
dahoang2311 <dahoang231198@gmail.com>
2022-08-02 16:38:03 +0700

    ok

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+import { HeaderOnly } from '~/components/Layout';
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/search', component: Search },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+const privateRoutes = [];
+export { publicRoutes, privateRoutes };
